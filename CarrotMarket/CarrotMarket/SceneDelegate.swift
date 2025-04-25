bd30726fb5a4a2119fbd5c5aa13ba0bf2779c932
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-04-25 15:24:09 +0900

    2주차 세미나 공부 내용 - 클로저,Delegate

--- CarrotMarket/CarrotMarket/SceneDelegate.swift
+++ CarrotMarket/CarrotMarket/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             let window = UIWindow(windowScene: windowScene)
         
-            let loginViewController = LoginViewController()
+            let loginViewController = ScrollViewController()
             let navigationController = UINavigationController(rootViewController: loginViewController)
             window.rootViewController = navigationController
             window.makeKeyAndVisible()
